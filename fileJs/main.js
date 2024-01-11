2501673b7f0b128e810c864ef8d7efe3abbeef9f
DavideCiammetti <davideciammetti97@gmail.com>
2024-01-11 19:09:59 +0100

    fix: funzione per gestire il click e mostrare ulteriori dati

--- fileJs/main.js
+++ fileJs/main.js
@@ -4,14 +4,23 @@ createApp({
   data() {
     return {
       data: [],
+      showAll: false,
+      showData: [],
     };
   },
   methods:{
+    // chiamata axios
     getData() {
       axios.get('index.php').then((response) => {
         this.data = response.data;
       });
   },
+  // funzione per mostrare altri dati al click 
+  showInfo(index){
+    this.data.forEach((song, i) => {
+       song.show = (i === index) ? !song.show : false;
+  });
+  }
 },
   created(){
     this.getData();
