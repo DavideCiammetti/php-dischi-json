832fb05918acd0302aadb6fdb17d375103ec8c07
DavideCiammetti <davideciammetti97@gmail.com>
2024-01-15 23:02:39 +0100

    fix: correzione e utilizzo di post per prendere dati

--- fileJs/main.js
+++ fileJs/main.js
@@ -4,21 +4,60 @@ createApp({
   data() {
     return {
       data: [],
+      allData: null,
+      apiUrl: 'index.php',
+      newForm: false,
+      nImg: '',
+      nName: '',
+      nType: '',
+      nSinger: '',
     };
   },
   methods:{
     // chiamata axios
     getData() {
-      axios.get('index.php').then((response) => {
+      axios.get(this.apiUrl).then((response) => {
         this.data = response.data;
       });
   },
   // funzione per mostrare altri dati al click 
   showInfo(index){
     this.data.forEach((song, i) => {
-       song.show = (i === index) ? !song.show : false;
+       if(i === index){
+        song.show = !song.show;
+       }else{
+        song.show;
+       }
   });
-  }
+  },
+  // prendo elementi in post
+  addDiscs(){
+    const param = {
+      create: 1,
+      newImg: this.nImg,
+      newName: this.nName,
+      newType: this.nType,
+      newSinger: this.nSinger,
+    };
+    axios.post(this.apiUrl, param,{ 
+      headers:{
+        'Content-Type': 'multipart/form-data',
+      },
+     }).then((response)=>{
+      this.data = response.data;
+      console.log(this.data);
+     });
+
+     this.nImg = '';
+     this.nName = '';
+     this.nType = '';
+     this.nSinger = '';
+  },
+  // funzione mostra form
+  showForm(){
+    this.newForm = !this.newForm;
+    return this.newForm;
+  },
 },
   created(){
     this.getData();
--- index.php
+++ index.php
@@ -1,11 +1,34 @@
 <?php
 
-include_once __DIR__. '/script.php';
+// prendo i dati da json
+$database = file_get_contents(__DIR__.'/data.json');
 
+// trasformo i dati in php
+$discs = json_decode($database);
 
-$string = file_get_contents('data.json');
+$result = $discs;
 
-$data = json_decode($string);
+ // creo nuovi elementi per il mio db 
+ if(isset($_POST['create'])){
+        
+    $newImg = $_POST['newImg']; 
+    $newName = $_POST['newName'];
+    $newType = $_POST['newType'];
+    $newSinger = $_POST['newSinger'];
+    
+    $newData = [
+        'img'=> $newImg,
+        'name'=>  $newName,
+        'show'=> false,
+        'type'=> $newType,
+        'singers'=> $newSinger,
+    ];
+    $discs[] = $newData;
+
+    // inserisco i dati nel json codificandoli in json 
+    file_put_contents('data.json',json_encode($discs));
+}
+
 
 header('Content-Type: application/json');
-echo json_encode($data);
+echo json_encode($discs);
